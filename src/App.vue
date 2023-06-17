c1fafdf421bbff7c03c67049cb2f482bea8c5f00
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2023-06-17 09:47:04 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container py-4">
+    <Home />
+  </div>
+</template>
+
+<script>
+import Home from "./components/Home.vue";
+
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
