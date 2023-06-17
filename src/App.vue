fd954f67075f7652aa00a09c60a116a09166bf2a
Junaidi Abdul Rahman <junaidiar.fcb@gmail.com>
2023-06-17 10:14:02 +0800

    install vue router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,41 @@
 <template>
+  <nav class="navbar navbar-expand-lg bg-danger" data-bs-theme="dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">Belajar Vue.js</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link
+              class="nav-link active"
+              aria-current="page"
+              :to="{ name: 'home' }"
+              >Home
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
   <div class="container py-4">
-    <Home />
+    <router-view></router-view>
   </div>
 </template>
 
-<script>
-import Home from "./components/Home.vue";
-
-export default {
-  components: {
-    Home,
-  },
-};
-</script>
+<script></script>
